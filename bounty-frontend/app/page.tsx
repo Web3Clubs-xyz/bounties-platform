45f290217a39b8c387402d2e1555e1bf9959f453
anthonykimani <kimaniantoni@gmail.com>
2024-05-26 01:24:17 +0300

    feat: Adds Dashboard Component

--- bounty-frontend/app/page.tsx
+++ bounty-frontend/app/page.tsx
@@ -1,9 +1,10 @@
+import DashboardComponent from "@/components/dashboard/Dashboard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      
+      <DashboardComponent />
     </main>
   );
 }
