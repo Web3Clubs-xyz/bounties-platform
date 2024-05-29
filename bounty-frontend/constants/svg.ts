957fd88b8137543657e953972a1ba4b99ed63175
anthonykimani <kimaniantoni@gmail.com>
2024-05-29 14:17:15 +0300

    feat: Adds Navbar components

---
+++ bounty-frontend/constants/svg.ts
@@ -0,0 +1,7 @@
+import Web3 from "@/public/svg/web3-logo.svg";
+import UserProfile from "@/public/svg/user-profile.svg";
+
+export {
+    Web3,
+    UserProfile
+}
---
+++ bounty-frontend/layout/Navbar.tsx
@@ -0,0 +1,237 @@
+"use client";
+
+import { Fragment } from "react";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from "@headlessui/react";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import { UserProfile, Web3 } from "@/constants/svg";
+
+const user = {
+  name: "Tom Cook",
+  email: "tom@example.com",
+  imageUrl:
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+
+const navigation = [
+  { name: "Bounties", href: "#", current: true },
+  { name: "Projects", href: "#", current: false },
+  { name: "Hackathons", href: "#", current: false },
+];
+
+const userNavigation = [
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+const Navbar = () => {
+  return (
+    <>
+      <Disclosure
+        as="nav"
+        className="border-b border-indigo-300 border-opacity-25 lg:border-none"
+      >
+        {({ open }) => (
+          <>
+            <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+              <div className="relative flex h-16 items-center justify-between lg:border-b lg:border-indigo-400 lg:border-opacity-25">
+                <div className="flex items-center px-2 lg:px-0">
+                  <div className="flex-shrink-0">
+                    <Image
+                      className="block w-[100px]"
+                      src={Web3}
+                      alt="Your Company"
+                      width={0}
+                      height={0}
+                    />
+                  </div>
+                  <div className="hidden lg:ml-10 lg:block">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-indigo-700 text-white"
+                              : "text-white hover:bg-indigo-500 hover:bg-opacity-75",
+                            "rounded-md py-2 px-3 text-sm font-medium"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-1 justify-center px-2 lg:ml-6 lg:justify-end">
+                  <div className="w-full max-w-lg lg:max-w-xs">
+                    <label htmlFor="search" className="sr-only">
+                      Search
+                    </label>
+                    <div className="relative text-gray-400 focus-within:text-gray-600">
+                      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                        <MagnifyingGlassIcon
+                          className="h-5 w-5"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <input
+                        id="search"
+                        className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="Search"
+                        type="search"
+                        name="search"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex lg:hidden">
+                  {/* Mobile menu button */}
+                  <DisclosureButton className="inline-flex items-center justify-center rounded-md bg-indigo-600 p-2 text-indigo-200 hover:bg-indigo-500 hover:bg-opacity-75 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </DisclosureButton>
+                </div>
+                <div className="hidden lg:ml-4 lg:block">
+                  <div className="flex items-center">
+                    <button
+                      type="button"
+                      className="flex-shrink-0 rounded-full bg-indigo-600 p-1 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600"
+                    >
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+
+                    {/* Profile dropdown */}
+                    <Menu as="div" className="relative ml-3 flex-shrink-0">
+                      <div>
+                        <MenuButton className="flex rounded-full bg-indigo-600 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600">
+                          <span className="sr-only">Open user menu</span>
+                          <Image
+                            className="h-8 w-8 rounded-full"
+                            src=""
+                            alt=""
+                          />
+                        </MenuButton>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {userNavigation.map((item) => (
+                            <MenuItem key={item.name}>
+                              {({ focus }) => (
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    focus ? "bg-gray-100" : "",
+                                    "block px-4 py-2 text-sm text-gray-700"
+                                  )}
+                                >
+                                  {item.name}
+                                </a>
+                              )}
+                            </MenuItem>
+                          ))}
+                        </MenuItems>
+                      </Transition>
+                    </Menu>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <DisclosurePanel className="lg:hidden">
+              <div className="space-y-1 px-2 pb-3 pt-2">
+                {navigation.map((item) => (
+                  <DisclosureButton
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-indigo-700 text-white"
+                        : "text-white hover:bg-indigo-500 hover:bg-opacity-75",
+                      "block rounded-md py-2 px-3 text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </DisclosureButton>
+                ))}
+              </div>
+              <div className="border-t border-indigo-700 pb-3 pt-4">
+                <div className="flex items-center px-5">
+                  <div className="flex-shrink-0">
+                    <Image
+                      className="h-10 w-10 rounded-full"
+                      src={UserProfile}
+                      alt=""
+                      width={0}
+                      height={0}
+                    />
+                  </div>
+                  <div className="ml-3">
+                    <div className="text-base font-medium text-white">
+                      {user.name}
+                    </div>
+                    <div className="text-sm font-medium text-indigo-300">
+                      {user.email}
+                    </div>
+                  </div>
+                  <button
+                    type="button"
+                    className="ml-auto flex-shrink-0 rounded-full bg-indigo-600 p-1 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600"
+                  >
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
+                <div className="mt-3 space-y-1 px-2">
+                  {userNavigation.map((item) => (
+                    <DisclosureButton
+                      key={item.name}
+                      as="a"
+                      href={item.href}
+                      className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-indigo-500 hover:bg-opacity-75"
+                    >
+                      {item.name}
+                    </DisclosureButton>
+                  ))}
+                </div>
+              </div>
+            </DisclosurePanel>
+          </>
+        )}
+      </Disclosure>
+    </>
+  );
+};
+
+export default Navbar;
