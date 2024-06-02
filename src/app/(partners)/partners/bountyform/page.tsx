a1be93467ffe7a4e8a668573325e2c1f71eeb05b
anthonykimani <kimaniantoni@gmail.com>
2024-06-02 10:57:36 +0300

    feat: updates partners page

--- src/app/(partners)/partners/bountyform/page.tsx
+++ src/app/(partners)/partners/bountyform/page.tsx
@@ -77,7 +77,7 @@ const BountyForm: React.FC = () => {
             type="submit"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-full"
           >
-            Create and Publish Bounty
+            Submit Bounty for Review
           </button>
         )}
         {current > 0 && (
