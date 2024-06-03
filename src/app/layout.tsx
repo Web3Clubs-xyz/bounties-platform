c58c474d2545f2a2458f2b90e2a5495a2a66a285
Dennis Kimathi <kimathidennis38@gmail.com>
2024-06-03 14:22:27 +0300

    remove hydration errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Lexend } from "next/font/google"
+import { Lexend } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-
+import ClientOnly from "@/components/common/client_only";
 
 const lexend = Lexend({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Web3Clubs Bounties",
-  description: "Bounties and projects for freelancers and developers in the Web3 space.",
+  description:
+    "Bounties and projects for freelancers and developers in the Web3 space.",
 };
 
 export default function RootLayout({
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={lexend.className}>
-        <Toaster/>
-        {children}
-        </body>
+        <ClientOnly>
+          <Toaster />
+          {children}
+        </ClientOnly>
+      </body>
     </html>
   );
 }
