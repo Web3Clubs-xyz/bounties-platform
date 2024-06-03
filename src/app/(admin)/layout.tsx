e95f4d319cc0031acd0a94252da4830c3d18a65e
anthonykimani <kimaniantoni@gmail.com>
2024-06-03 12:14:57 +0300

    feat: creates admin page

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import Nav from "@/components/common/nav/nav";
+import { Lexend } from "next/font/google";
+import SideBar from "@/components/common/sidebar/sidebar";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+
+export const metadata: Metadata = {
+  title: "Bounties",
+  description: "Bounties for freelancers and developers in the Web3 space.",
+};
+
+const lexend = Lexend({ subsets: ["latin"] });
+
+export default function PartnersPagesLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full bg-white">
+      <body className={lexend.className}>
+        <div className="min-h-full">
+          <div className="   pb-32">
+            <AntdRegistry>
+              <SideBar>{children}</SideBar>
+            </AntdRegistry>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
